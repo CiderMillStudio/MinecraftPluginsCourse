9db7069ae6eacbb0742a029b0c068ac9729b85e8
WadyJ@Omen <cidermillstudio1@gmail.com>
2026-06-09 15:11:21 -0400

    All set and ready to crush this Minecraft Plugins course :)))

---
+++ Learning_Spigot/learning02_spigot_commands/src/main/java/com/wady/learning02_spigot_commands/Main.java
@@ -0,0 +1,17 @@
+package com.wady.learning02_spigot_commands;
+
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Main extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
