62fe5a525b1f17e30403119be0ce72b21c3b4147
WadyJ@Omen <cidermillstudio1@gmail.com>
2026-06-18 18:12:21 -0400

    onto debugging and errors

---
+++ Learning_Spigot/learning05_debugging_and_errors/src/main/java/com/wady/learning05_debugging_and_errors/Main.java
@@ -0,0 +1,16 @@
+package com.wady.learning05_debugging_and_errors;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Main extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
